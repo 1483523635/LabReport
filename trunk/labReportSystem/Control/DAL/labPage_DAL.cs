09a54c7720a8a2b7495cf5abe761c7905bda10f7
曲廉卿 <1483523635@qq.com>
2017-03-30 18:29:00 +0800

    a method added

---
+++ trunk/labReportSystem/Control/DAL/labPage_DAL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace labReportSystem.Control.DAL
+{
+    public class labPage_DAL
+    {
+        public bool CommitData(string teaname, string location, string year)
+        {
+            string sql = "insert into ";
+            return new SqlDB().ExecSql(sql);
+
+        }
+    }
+}
